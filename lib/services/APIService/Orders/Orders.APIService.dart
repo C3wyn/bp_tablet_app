d282b3aa56640df585507395a867192dbc9ee008
C3wyn <kevinnikol17@gmail.com>
2023-10-02 12:15:39 +0200

    updated createOrder

--- lib/services/APIService/Orders/Orders.APIService.dart
+++ lib/services/APIService/Orders/Orders.APIService.dart
@@ -7,68 +7,68 @@ import 'package:bp_tablet_app/models/Order/orderitem.model.dart';
 import 'package:bp_tablet_app/models/Order/orderstatus.enum.dart';
 import 'package:bp_tablet_app/models/extra.model.dart';
 import 'package:bp_tablet_app/models/ingredient.model.dart';
+import 'package:bp_tablet_app/models/product.model.dart';
 import 'package:bp_tablet_app/services/APIService/Models/apiresponse.model.dart';
 import 'package:bp_tablet_app/services/CartService/Cart.service.dart';
 import 'package:http/http.dart' as http;
+
 class OrdersAPIService {
-  Map<String,String> headers = {
-    'Content-Type':'application/json',
+  Map<String, String> headers = {
+    'Content-Type': 'application/json',
     'Accept': 'application/json'
   };
   Future<APIResponse> addOrder(BPOrder order) async {
     String body = _orderToJson(order);
     var response = await http.post(
-      Uri.parse('http://localhost:1337/backpoint/createOrder'),
-      headers: headers,
-      body: body
-    );
+        Uri.parse('http://${BPEnvironment.BASEURL}/createOrder'),
+        headers: headers,
+        body: body);
     Map<String, dynamic> data = jsonDecode(response.body);
-    if(response.statusCode==200){
-      CartService.currentOrder = BPOrder(status: OrderStatus.Created, orderItems: []);
+    print(data);
+    print(response.statusCode);
+    if (data['acknowledged'] == true && response.statusCode == 200) {
+      CartService.currentOrder =
+          BPOrder(status: OrderStatus.Created, orderItems: []);
       return APIResponse(
-        200,
-        "Successfull", 
-        "Produkt erfolgreich hinzugefügt",
-        null
-      );
+          200, "Successfull", "Produkt erfolgreich hinzugefügt", null);
     }
-    return APIResponse(data['error']['status'], data['error']['name'], data['error']['message'], null);
+    return APIResponse(data['error']['status'], data['error']['name'],
+        data['error']['message'], null);
   }
-  
+
   String _orderToJson(BPOrder order) {
     var items = [];
-    for(BPOrderItem item in order.orderItems){
+    for (BPOrderItem item in order.orderItems) {
       var ingredients = [];
-      for(BPIngredient ingredient in item.selectedIngredients.keys.where((x) => !item.selectedIngredients[x]!)){
-        ingredients.add(
-          {
-            "Name": ingredient.Name,
-            "Selected": item.selectedIngredients[ingredient],
-            "Default": true
-          }
-        );
+      for (BPIngredient ingredient in item.selectedIngredients.keys
+          .where((x) => !item.selectedIngredients[x]!)) {
+        ingredients.add({
+          "Name": ingredient.Name,
+          "Selected": item.selectedIngredients[ingredient],
+          "Default": true
+        });
       }
       var extras = [];
-      for(BPExtra extra in item.selectedExtras.keys.where((x) => item.selectedExtras[x]!)){
+      for (BPExtra extra
+          in item.selectedExtras.keys.where((x) => item.selectedExtras[x]!)) {
         extras.add({"Name": extra.Name});
       }
-
       items.add({
-        "productID": item.product.ID,
-        "selectedIngredients": ingredients,
-        "selectedExtras": extras,
-        "customerDescription": item.customerDescription
+        "_id": item.product.ID,
+        "Name": item.product.Name,
+        "Price": item.product.Price,
+        "Description": item.product.Description,
+        "SelectedIngredients": ingredients,
+        "SelectedExtras": extras
       });
     }
-    
     return jsonEncode({
-      "data": {
-        "id": order.id,
-        "items": items,
-        "pickUpDate": order.pickUpDate?? DateTime.now().toUtc().toString(),
-        "deliveryType": order.deliveryType==OrderDeliveryType.EatHere? 2: 1,
-        "orderDescription": order.orderDescription
-      }
+      "items": items,
+      "pickUpDate": order.pickUpDate ?? DateTime.now().toUtc().toString(),
+      "deliveryType": order.deliveryType == OrderDeliveryType.EatHere
+          ? "Eat Here"
+          : "Take Away",
+      "orderDescription": order.orderDescription
     });
   }
 }
